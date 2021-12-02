77e13bf275f71feafcb9a61d3480b69c05cdc220
lordsherman <kyleeverling5244@gmail.com>
2021-12-02 05:30:28 -0500

    Revert Test Commit

--- untitled.py
+++ untitled.py
@@ -19,7 +19,7 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#TEEST TEST TEST
+
 import salt.config
 import salt.loader
 import salt.client
